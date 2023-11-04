a2de4611486305693f3f0b739838047450fa2dd9
313974 <313974@cs.ktu.edu.tr>
2023-11-05 01:29:52 +0300

    Add project files.

---
+++ LookingforYatchs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LookingforYatchs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
