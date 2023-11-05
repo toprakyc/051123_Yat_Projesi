61861b96655979b188e531ef97fa00aae24c71fe
313974 <313974@cs.ktu.edu.tr>
2023-11-06 01:29:54 +0300

    ufak değişiklikler eklemeler

--- LookingforYatchs/Controllers/HomeController.cs
+++ LookingforYatchs/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace LookingforYatchs.Controllers
         {
             return View();
         }
+        public IActionResult Index1()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
