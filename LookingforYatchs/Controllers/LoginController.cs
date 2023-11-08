f61f79a4d0acc33a588742a9d4eaa792526c24e7
313974 <313974@cs.ktu.edu.tr>
2023-11-08 05:25:58 +0300

    LoginController eklendi Save view i eklendi Index1 actionu ve indexi iptal edildi

---
+++ LookingforYatchs/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using LookingforYatchs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LookingforYatchs.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Save()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Save(Kayıt model)
+        {
+            return View();
+        }
+    }
+}
---
+++ LookingforYatchs/Models/Kayıt.cs
@@ -0,0 +1,15 @@
+﻿namespace LookingforYatchs.Models
+{
+    public class Kayıt
+    {
+        public String? Ad { get; set; }
+        public String? Soyad { get; set; }
+        public int Yas { get; set; }
+        public DateTime KayıtTarihi { get; set; }
+
+        public Kayıt()
+        {
+                KayıtTarihi= DateTime.Now;
+        }
+    }
+}
